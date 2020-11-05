a3054d30fe29b3baad0026f55599587b90fbd839
Aleksandr Ermakov <aleksandr.ermakov@tularegion.ru>
2020-11-05 23:04:39 +0300

    Added all project files

---
+++ js/main.js
@@ -0,0 +1,161 @@
+/**
+ * Array.map() polyfill
+ */
+// From https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
+// Production steps of ECMA-262, Edition 5, 15.4.4.19
+// Reference: http://es5.github.io/#x15.4.4.19
+if (!Array.prototype.map) {
+
+  Array.prototype.map = function(callback/*, thisArg*/) {
+
+    var T, A, k;
+
+    if (this == null) {
+      throw new TypeError('this is null or not defined');
+    }
+
+    // 1. Let O be the result of calling ToObject passing the |this|
+    //    value as the argument.
+    var O = Object(this);
+
+    // 2. Let lenValue be the result of calling the Get internal
+    //    method of O with the argument "length".
+    // 3. Let len be ToUint32(lenValue).
+    var len = O.length >>> 0;
+
+    // 4. If IsCallable(callback) is false, throw a TypeError exception.
+    // See: http://es5.github.com/#x9.11
+    if (typeof callback !== 'function') {
+      throw new TypeError(callback + ' is not a function');
+    }
+
+    // 5. If thisArg was supplied, let T be thisArg; else let T be undefined.
+    if (arguments.length > 1) {
+      T = arguments[1];
+    }
+
+    // 6. Let A be a new array created as if by the expression new Array(len)
+    //    where Array is the standard built-in constructor with that name and
+    //    len is the value of len.
+    A = new Array(len);
+
+    // 7. Let k be 0
+    k = 0;
+
+    // 8. Repeat, while k < len
+    while (k < len) {
+
+      var kValue, mappedValue;
+
+      // a. Let Pk be ToString(k).
+      //   This is implicit for LHS operands of the in operator
+      // b. Let kPresent be the result of calling the HasProperty internal
+      //    method of O with argument Pk.
+      //   This step can be combined with c
+      // c. If kPresent is true, then
+      if (k in O) {
+
+        // i. Let kValue be the result of calling the Get internal
+        //    method of O with argument Pk.
+        kValue = O[k];
+
+        // ii. Let mappedValue be the result of calling the Call internal
+        //     method of callback with T as the this value and argument
+        //     list containing kValue, k, and O.
+        mappedValue = callback.call(T, kValue, k, O);
+
+        // iii. Call the DefineOwnProperty internal method of A with arguments
+        // Pk, Property Descriptor
+        // { Value: mappedValue,
+        //   Writable: true,
+        //   Enumerable: true,
+        //   Configurable: true },
+        // and false.
+
+        // In browsers that support Object.defineProperty, use the following:
+        // Object.defineProperty(A, k, {
+        //   value: mappedValue,
+        //   writable: true,
+        //   enumerable: true,
+        //   configurable: true
+        // });
+
+        // For best browser support, use the following:
+        A[k] = mappedValue;
+      }
+      // d. Increase k by 1.
+      k++;
+    }
+
+    // 9. return A
+    return A;
+  };
+}
+
+
+// toggle sidebar
+const toggleSidebar = document.querySelector('.toggle-sidebar'),
+  menuIcon = document.querySelector('.menu-icon'),
+  sidebar = document.querySelector('.sidebar');
+
+toggleSidebar.addEventListener('click', () => {
+  menuIcon.classList.toggle('menu-icon-active');
+  sidebar.classList.toggle('sidebar--mobile-active');
+})
+
+// show hidden cards
+const showHiddenCardsBtn = document.querySelector('.btn-more'),
+  hiddenCards = [...document.querySelectorAll('.card.card--hidden')];
+
+showHiddenCardsBtn.addEventListener('click', function() {
+  hiddenCards.map(card => card.classList.remove('card--hidden'));
+  this.style.display = 'none';
+});
+
+// toggle widgets content inside filter block
+const widgets = document.querySelectorAll('.widget');
+
+widgets.forEach(function (widget) {
+  widget.addEventListener('click', function (e) {
+    if (e.target.classList.contains('widget__title')) {
+      e.target.classList.toggle('widget__title--active');
+      e.target.nextElementSibling.classList.toggle('widget__body--hidden');
+    }
+  })
+})
+
+// clear all checkboxes on #distance-any click
+const distanceAnyBtn = document.querySelector('#distance-any'),
+  distanceCheckboxes = [...document.querySelectorAll('[data-distance-param]')];
+
+distanceAnyBtn.addEventListener('change', function () {
+  if (this.checked) {
+    distanceCheckboxes.map(checkbox => checkbox.checked = false);
+  }
+})
+
+// clear #distance-any checkbox on any data-distance-param checkbox click
+distanceCheckboxes.forEach(checkbox => {
+  checkbox.addEventListener('change', () => {
+    if (distanceAnyBtn.checked) {
+      distanceAnyBtn.checked = false;
+    }
+  })
+})
+
+// show hidden checkboxes
+const showHiddenCheckboxesBtn = document.querySelector('.widget__btn-show-hidden'),
+  hiddenCheckboxes = [...document.querySelectorAll('.checkbox.checkbox--hidden')];
+
+showHiddenCheckboxesBtn.addEventListener('click', function () {
+  if (this.dataset.options === 'hidden') {
+    hiddenCheckboxes.map(checkbox => checkbox.style.display = 'block');
+    this.dataset.options = 'visible';
+    this.innerText = 'Скрыть дополнительные опции';
+  } else if (this.dataset.options === 'visible') {
+    hiddenCheckboxes.map(checkbox => checkbox.style.display = 'none');
+    this.dataset.options = 'hidden';
+    this.innerText = 'Показать ещё';
+  }
+
+})
